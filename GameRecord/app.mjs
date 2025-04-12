b331b8657d2d0ccce8e3a0b008ce123ebacf0ba3
TurboSerb <mkljaic4@gmail.com>
2025-04-12 23:06:54 +0200

    Step 3

--- GameRecord/app.mjs
+++ GameRecord/app.mjs
@@ -1,1 +1,18 @@
-import Class Game from "./models/Game.mjs";
+import Game from "./models/Game.mjs";
+
+const STORAGE_NAME = "GameData";
+function saveLocalStorage(gameObjects) {
+    localStorage.setItem(STORAGE_NAME, gameObjects);
+}
+
+function retriveGamesFromLocalStorage() {
+    return localStorage.getItem(STORAGE_NAME);
+}
+
+function outputJson(jsonstring) { 
+    return JSON.parse(jsonstring);
+}
+
+function saveJSON(gameObjects) {
+    saveToStorage(JSON.stringify(gameObjects));
+}
