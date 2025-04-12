7acaa149120692474035b27204d9f05832ac1c04
TurboSerb <mkljaic4@gmail.com>
2025-04-12 23:56:45 +0200

    Step 4

--- GameRecord/app.mjs
+++ GameRecord/app.mjs
@@ -1,11 +1,36 @@
 import Game from "./models/Game.mjs";
 
 const STORAGE_NAME = "GameData";
+let games = retrieveAllGames()
+
+function retrieveAllGames() {
+    let listOfRetrievedGames = outputJson(retrieveGamesFromLocalStorage()||"[]");
+    let result = [];
+
+    for(let i=0;i<listOfRetrievedGames.length;i++) {
+        let entry = listOfRetrievedGames[i];
+        result.push(new Game(
+            entry.title,
+            entry.designer,
+            entry.artist,
+            entry.publisher, 
+            entry.year,
+            entry.players,
+            entry.time,
+            entry.difficulty,
+            entry.url,
+            entry.playCount,
+            entry.personalRating
+        ));
+    }
+    return result;
+}
+
 function saveLocalStorage(gameObjects) {
     localStorage.setItem(STORAGE_NAME, gameObjects);
 }
 
-function retriveGamesFromLocalStorage() {
+function retrieveGamesFromLocalStorage() {
     return localStorage.getItem(STORAGE_NAME);
 }
 
@@ -16,3 +41,12 @@ function outputJson(jsonstring) {
 function saveJSON(gameObjects) {
     saveToStorage(JSON.stringify(gameObjects));
 }
+
+document.getElementById("importSource").onchange = function(event) {
+    let file = event.target.files[0]; 
+    const reader = new FileReader();
+    reader.onload = (evt) => {
+    saveLocalStorage(evt.target.result);
+    };
+    reader.readAsText(file);
+}; 
---
+++ GameRecord/readme.md
@@ -0,0 +1,15 @@
+Step 1. 
+
+created folder and made a git repo. Added basic html 5 scaffoldd
+
+Step 2
+
+created a class called game, which we exported from game.mjs and imported into app.mjs. there is the information based on the example.JSON. title, designer, players, ect.
+
+Step 3.
+Created four functions, one to save the game to local storage, create a function that retrieves all games saved in local storage, a function that outputs all games as JSON and a function that imports the mentioned JSON.
+
+Step 4
+
+created a input element and gave it the ability to save it to local storage. created another function to give all of the game entries and put them into the class object.
+
